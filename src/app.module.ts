5b87a7eb83db209f62d22cf5d481bfabe19b3c44
Yubisel Vega <yubiselv@gmail.com>
2024-04-29 14:33:04 -0300

    Add @nestjs/mapped-types package to package.json and pnpm-lock.yaml

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,8 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { dbConfig } from './config/db.config';
+import { ProductsModule } from './products/products.module';
+import { TenantsModule } from './tenants/tenants.module';
 
 @Module({
   imports: [
@@ -12,6 +14,8 @@ import { dbConfig } from './config/db.config';
       cache: true,
       load: [dbConfig],
     }),
+    ProductsModule,
+    TenantsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
